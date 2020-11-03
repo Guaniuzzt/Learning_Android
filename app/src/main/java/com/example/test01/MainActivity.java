26471e069f26430d041fd07e8f68e075a545e411
Zetao Zhu <georgezzt@outlook.com>
2020-11-02 20:09:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.test01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "I have been clicked.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
