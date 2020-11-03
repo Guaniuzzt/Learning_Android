c15288e8e1b03cbd243d8d79935c0da4d05aa920
Zetao Zhu <georgezzt@outlook.com>
2020-11-03 00:30:26 -0500

    4 lessons finished

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -2,23 +2,80 @@ package com.example.test01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int RESULT_CODE = 1234;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "I have been clicked.", Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, "I have been clicked.", Toast.LENGTH_LONG).show();
+               /* Intent intent = new Intent();
+                intent.putExtra(SplashActivity.TITLE, "Present from MainActivity.");
+                setResult(RESULT_CODE, intent);
+                finish();*/
+
+                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(intent);
             }
         });
+
+        Intent intent = getIntent();
+        if(intent != null){
+            String title = intent.getStringExtra(SplashActivity.TITLE);
+            UserInfo userInfo = (UserInfo) intent.getSerializableExtra(SplashActivity.USER_INFO);
+            setTitle("The name of XiaoMin: " + userInfo.getmAge());
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
     }
 }
---
+++ app/src/main/java/com/example/test01/UserInfo.java
@@ -0,0 +1,31 @@
+package com.example.test01;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+
+
+    private String mUserName;
+    private int mAge;
+
+    public UserInfo(String mUserName, int mAge) {
+        this.mUserName = mUserName;
+        this.mAge = mAge;
+    }
+
+    public String getmUserName() {
+        return mUserName;
+    }
+
+    public int getmAge() {
+        return mAge;
+    }
+
+    public void setmUserName(String mUserName) {
+        this.mUserName = mUserName;
+    }
+
+    public void setmAge(int mAge) {
+        this.mAge = mAge;
+    }
+}
